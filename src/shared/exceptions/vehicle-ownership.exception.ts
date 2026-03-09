7ba22d67fed9a13d265eaeed104d862c6ee6bf25
Julio Rios <julio.rios.araujo@gmail.com>
2026-03-09 20:20:33 -0300

    adding exceptions

---
+++ src/shared/exceptions/vehicle-ownership.exception.ts
@@ -0,0 +1,10 @@
+import { DomainException } from './domain.exception';
+
+export class VehicleOwnershipException extends DomainException {
+  constructor(vehicleId: string, customerId: string) {
+    super(
+      `Vehicle with ID "${vehicleId}" does not belong to customer with ID "${customerId}"`,
+    );
+    this.name = 'VehicleOwnershipException';
+  }
+}
